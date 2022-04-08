a53b901ac1e5bd49fc528ca7606c359755eb738c
strong-prog <strong.tretyakov@gmail.com>
2022-04-08 18:40:08 +0300

    дополнительные измененя

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,8 @@
+from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 
-link = 'http://selenium1py.pythonanywhere.com/'
-
-
-def go_to_link_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, '#login_link')
-    login_link.click()
-
-
-def test_quest_can_go_to_login_link(browser):
-    browser.get(link)
-    go_to_link_page(browser)
+def test_quest_can_go_to_login_page(browser):
+    link = 'http://selenium1py.pythonanywhere.com/'
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_link()
