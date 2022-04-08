84489704825d36925d4c39de97d4b161ac6a8eaf
strong-prog <strong.tretyakov@gmail.com>
2022-04-08 14:23:41 +0300

    создана отдельная функция go_to_link_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,13 @@
 from selenium.webdriver.common.by import By
 
+link = 'http://selenium1py.pythonanywhere.com/'
 
-def test_quest_can_go_to_login_link(browser):
-    link = 'http://selenium1py.pythonanywhere.com/'
-    browser.get(link)
+
+def go_to_link_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, '#login_link')
     login_link.click()
+
+
+def test_quest_can_go_to_login_link(browser):
+    browser.get(link)
+    go_to_link_page(browser)
